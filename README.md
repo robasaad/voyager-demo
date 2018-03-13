8d1372f42a02d806cf734b280463fa9df3f510d8
Robert Asaad <robert@cumulusnetworks.com>
2018-03-12 17:16:10 -0700

    set up the bare bones dev environment

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# voyager-ui
+# Voyager Demo
+
+
+## Prerequisites
+
+- Node.js (version 9.x)
+- npm (version 5.x)
+
+
+## Installation
+
+```
+git clone git@github.com:robasaad/voyager-demo.git
+cd voyager-demo
+```
+
+
+## Build
+
+```
+npm install
+```
+
+
+## Run server
+
+Run the built-in Node.js web server by typing:
+
+```
+node server.js
+```
+
+and then go to http://localhost:9000 in your browser.
+
+
+## Compiling Sass to CSS
+
+- To compile the main Sass file to CSS, use `gulp sass`.
+- To set an automated task that compiles CSS when changes are made to the main Sass file, use `gulp watch`.
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+
+var autoprefixerOptions = {
+  browsers: [
+      "Android 2.3",
+      "Android >= 4",
+      "Chrome >= 20",
+      "Firefox >= 24",
+      "Explorer >= 8",
+      "iOS >= 6",
+      "Opera >= 12",
+      "Safari >= 6"
+  ]
+};
+
+gulp.task('sass', function() {
+  return gulp
+  .src('./scss/styles.scss')
+    .pipe(sass())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(cssnano())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./scss/styles.scss', ['sass']);
+});
